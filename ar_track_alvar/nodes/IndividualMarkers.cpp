eadaf42a05afcf2180801c9d4726fe8690e384f8
Zach Z-V <zzv2@cornell.edu>
2018-04-12 10:43:53 -0700

    Fixed nan issue preventing translation improvement.

--- ar_track_alvar/nodes/IndividualMarkers.cpp
+++ ar_track_alvar/nodes/IndividualMarkers.cpp
@@ -198,53 +198,55 @@ int PlaneFitPoseImprovement(int id, const ARCloud &corners_3D, ARCloud::Ptr sele
 
   draw3dPoints(selected_points, cloud.header.frame_id, 1, id, 0.005);
 
-  //Get 2 points that point forward in marker x direction
-  int i1,i2;
-  if(isnan(corners_3D[0].x) || isnan(corners_3D[0].y) || isnan(corners_3D[0].z) ||
-     isnan(corners_3D[3].x) || isnan(corners_3D[3].y) || isnan(corners_3D[3].z))
-    {
-      if(isnan(corners_3D[1].x) || isnan(corners_3D[1].y) || isnan(corners_3D[1].z) ||
-	 isnan(corners_3D[2].x) || isnan(corners_3D[2].y) || isnan(corners_3D[2].z))
-	{
-	  return -1;
-	}
-      else{
-	i1 = 1;
-	i2 = 2;
-      }
-    }
-  else{
-    i1 = 0;
-    i2 = 3;
-  }
+ //  //Get 2 points that point forward in marker x direction
+ //  int i1,i2;
+ //  if(isnan(corners_3D[0].x) || isnan(corners_3D[0].y) || isnan(corners_3D[0].z) ||
+ //     isnan(corners_3D[3].x) || isnan(corners_3D[3].y) || isnan(corners_3D[3].z))
+ //    {
+ //      if(isnan(corners_3D[1].x) || isnan(corners_3D[1].y) || isnan(corners_3D[1].z) ||
+	//  isnan(corners_3D[2].x) || isnan(corners_3D[2].y) || isnan(corners_3D[2].z))
+	// {
+ //    ROS_ERROR("X dir points are nan.");
+	//   return -1;
+	// }
+ //      else{
+	// i1 = 1;
+	// i2 = 2;
+ //      }
+ //    }
+ //  else{
+ //    i1 = 0;
+ //    i2 = 3;
+ //  }
 
-  //Get 2 points the point forward in marker y direction
-  int i3,i4;
-  if(isnan(corners_3D[0].x) || isnan(corners_3D[0].y) || isnan(corners_3D[0].z) ||
-     isnan(corners_3D[1].x) || isnan(corners_3D[1].y) || isnan(corners_3D[1].z))
-    {
-      if(isnan(corners_3D[3].x) || isnan(corners_3D[3].y) || isnan(corners_3D[3].z) ||
-	 isnan(corners_3D[2].x) || isnan(corners_3D[2].y) || isnan(corners_3D[2].z))
-	{
-	  return -1;
-	}
-      else{
-	i3 = 2;
-	i4 = 3;
-      }
-    }
-  else{
-    i3 = 1;
-    i4 = 0;
-  }
+ //  //Get 2 points the point forward in marker y direction
+ //  int i3,i4;
+ //  if(isnan(corners_3D[0].x) || isnan(corners_3D[0].y) || isnan(corners_3D[0].z) ||
+ //     isnan(corners_3D[1].x) || isnan(corners_3D[1].y) || isnan(corners_3D[1].z))
+ //    {
+ //      if(isnan(corners_3D[3].x) || isnan(corners_3D[3].y) || isnan(corners_3D[3].z) ||
+	//  isnan(corners_3D[2].x) || isnan(corners_3D[2].y) || isnan(corners_3D[2].z))
+	// {
+ //    ROS_ERROR("Y dir points are nan.");
+	//   return -1;
+	// }
+ //      else{
+	// i3 = 2;
+	// i4 = 3;
+ //      }
+ //    }
+ //  else{
+ //    i3 = 1;
+ //    i4 = 0;
+ //  }
 
-  ARCloud::Ptr orient_points(new ARCloud());
-  orient_points->points.push_back(corners_3D[i1]);
-  draw3dPoints(orient_points, cloud.header.frame_id, 3, id+1000, 0.008);
+  // ARCloud::Ptr orient_points(new ARCloud());
+  // orient_points->points.push_back(corners_3D[i1]);
+  // draw3dPoints(orient_points, cloud.header.frame_id, 3, id+1000, 0.008);
 
-  orient_points->clear();
-  orient_points->points.push_back(corners_3D[i2]);
-  draw3dPoints(orient_points, cloud.header.frame_id, 2, id+2000, 0.008);
+  // orient_points->clear();
+  // orient_points->points.push_back(corners_3D[i2]);
+  // draw3dPoints(orient_points, cloud.header.frame_id, 2, id+2000, 0.008);
 
   // int succ;
   // succ = ata::extractOrientation(res.coeffs, corners_3D[i1], corners_3D[i2], corners_3D[i3], corners_3D[i4], pose.pose.orientation);
